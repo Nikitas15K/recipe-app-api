324e2da4755ecee5fc167908d5f95790a80e2e3b
Nikitas Kastis <nikitaskastis15@gmail.com>
2023-09-24 12:37:21 +0300

    recipe models

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,7 @@
 """"
 Database models
 """
-
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import ( AbstractBaseUser, BaseUserManager, PermissionsMixin,)
 
@@ -28,6 +28,7 @@ class UserManager(BaseUserManager):
         return user
 
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
     email = models.EmailField(max_length=255, unique=True)
@@ -38,3 +39,18 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects= UserManager()
 
     USERNAME_FIELD = 'email'
+
+class Recipe(models.Model):
+    """Recipe object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description=models.TextField(blank=True)
+    time_minutes=models.IntegerField()
+    price=models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
--- app/user/views.py
+++ app/user/views.py
@@ -22,9 +22,9 @@ class CreateTokenView(ObtainAuthToken):
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user."""
-    serializer_class= UserSerializer
-    authentication_classes= [authentication.TokenAuthentication]
-    permission_classes=[permissions.IsAuthenticated]
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes =[permissions.IsAuthenticated]
 
     def get_object(self):
         """Retrieve and return the authenticated user."""
